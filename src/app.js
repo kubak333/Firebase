7f2fc9052bbd0148fff9d3f01e300c031abbf40a
Jakub Kucharczyk <kubak333@gmail.com>
2022-11-22 18:25:34 +0100

    01 firebase

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import './../styles/styles.css'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASAg9KWLcUheVd5XvH49xrftHOafrXU0U",
+  authDomain: "sda-firebase-4f1cf.firebaseapp.com",
+  projectId: "sda-firebase-4f1cf",
+  storageBucket: "sda-firebase-4f1cf.appspot.com",
+  messagingSenderId: "420864867372",
+  appId: "1:420864867372:web:953590cc4cc78bafba6589"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
