9bd273eb9e83d8ee031233d8ef9da85278c6d039
Jakub Kucharczyk <kubak333@gmail.com>
2022-11-23 17:59:13 +0100

    03 download

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './../styles/styles.css'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +18,37 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+// const kubaRef = ref(storage, "6.png");
+// const img = document.getElementById("myImage");
+// img.src = `https://firebasestorage.googleapis.com/v0/b/${kubaRef.bucket}/o/${kubaRef.fullPath}?alt=media`
+
+const myBtn = document.getElementById("mySendBtn");
+myBtn.addEventListener("click", () => {
+    const myResult = document.getElementById("myResult");
+    myResult.innerText = "Przesyłam..."
+
+    const file = document.getElementById("myFileInput").files[0];
+    
+    if(file) {
+
+    const myFileNameInput = document.getElementById("myFileNameInput");
+    const myFileRef = ref(storage, file.value);
+    uploadBytes(myFileRef, file).then((result) => {
+        myResult.innerText = "Przesłano!"
+
+    })
+    }
+    else {
+        myResult.innerText = "Error";
+    }
+});
+
+
+
+//1. Po kliknięciu w elemencie h1 ma się pojawić info "Przesyłam"
+//2. Po zakończeniu przesyłania w elemencie h1 ma się pojawić info "Przesłano"
+//3. Dodajemy nowy input, w którym uzytkownik moe wpisać nazwę pliku
+//4. Plik razem z tą nazwą będzie przesłany
+//5. Jezeli uzytkownik nie wybrał pliku i klinął prześlij, to w elemencie h1 wyświetlamy "Error: proszę wybrać plik"
